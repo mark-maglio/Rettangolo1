5ef5f476ebc90099ee5668c37528baf5f0536933
mark maglio <markmaglio25@gmail.com>
2021-01-11 18:49:54 +0100

    non finito

---
+++ Rettangolo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rettangolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci i lati");
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci l'altezza");
+            int h = int.Parse(Console.ReadLine());
+            double area =  l*h/2;
+            double perimetro = (l + h) * 2;
+            Console.WriteLine($"L'area è {area} il perimetro è {perimetro}");
+        }
+    }
+}
